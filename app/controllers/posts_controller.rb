5ffea1a01610e7d06af591206b2ffb90afde74cf
k-yamada <yamadakazu45@gmail.com>
2014-01-01 14:24:24 +0900

    Fix destroy logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,8 +68,8 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post.destroy
     @post.delete_all_tags
+    @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url }
       format.json { head :no_content }
